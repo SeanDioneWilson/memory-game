8a957a4ad0547244a85af8a282b3a828f0f5e50e
Sean Wilson <seandionewilson@gmail.com>
2018-10-23 00:58:23 -0700

    created a clock function

--- js/app.js
+++ js/app.js
@@ -109,22 +109,11 @@ function hideStar() {
   }
 }
 
-
-
-
-
-
-
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// clock function using setInterval
+function startClock() {
+  time = 0;
+  let clockId = setInterval(() => {
+    time++
+    console.log(time);
+  }, 1000);
+}
